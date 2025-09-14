2e683aaf343193185754261d6778b1e546ed71f0
Dan Keegan <ironicrebel@outlook.com>
2025-09-14 23:10:53 +0100

    Add project files.

---
+++ Gigsy2.Server/Components/Account/IdentityUserAccessor.cs
@@ -0,0 +1,20 @@
+using Gigsy2.Server.Data;
+using Microsoft.AspNetCore.Identity;
+
+namespace Gigsy2.Server.Components.Account
+{
+    internal sealed class IdentityUserAccessor(UserManager<ApplicationUser> userManager, IdentityRedirectManager redirectManager)
+    {
+        public async Task<ApplicationUser> GetRequiredUserAsync(HttpContext context)
+        {
+            var user = await userManager.GetUserAsync(context.User);
+
+            if (user is null)
+            {
+                redirectManager.RedirectToWithStatus("Account/InvalidUser", $"Error: Unable to load user with ID '{userManager.GetUserId(context.User)}'.", context);
+            }
+
+            return user;
+        }
+    }
+}
